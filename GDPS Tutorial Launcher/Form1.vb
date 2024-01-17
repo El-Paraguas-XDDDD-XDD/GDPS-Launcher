7a3399c7e55df16842e6b3c82f0d399404245b42
El Paraguas XDDDDD <notdaysforme@gmail.com>
2024-01-17 15:05:00 -0500

    Update Form1.vb

--- GDPS Tutorial Launcher/Form1.vb
+++ GDPS Tutorial Launcher/Form1.vb
@@ -1,10 +1,10 @@
 ﻿Public Class Form1
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
         Dim gdpsFolderPath As String = IO.Path.Combine(Application.StartupPath, "gdps")
-
+' Gracias a @DimisAIO por arreglar el botón de "Launch"
         ' Set the working directory to the gdps folder
         Dim startInfo As New ProcessStartInfo()
-        startInfo.FileName = IO.Path.Combine(gdpsFolderPath, "KTNGDPS.exe")
+        startInfo.FileName = IO.Path.Combine(gdpsFolderPath, "YOURGDPS.exe") ' es necesario cambiar "YOURGDPS.exe" por el nombre de tu GDPS para que el launcher funcione
         startInfo.WorkingDirectory = gdpsFolderPath
 
         ' Start the process
@@ -15,14 +15,14 @@
     End Sub
 
     Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
-        Process.Start("https://twitter.com")
+        Process.Start("https://example.com")
     End Sub
 
     Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
-        Process.Start("https://instagram.com")
+        Process.Start("https://example.com")
     End Sub
 
     Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
-        Process.Start("https://youtube.com")
+        Process.Start("https://example.com")
     End Sub
 End Class
